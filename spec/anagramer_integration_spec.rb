01ba2f52299341e46dd0034d782755674df203ca
Michael & Drew <student@epicodus.com>
2015-04-23 14:43:24 -0700

    retroactively added integration test

--- spec/anagramer_integration_spec.rb
+++ spec/anagramer_integration_spec.rb
@@ -3,10 +3,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 
 describe('Index form', {:type => :feature}) do
-  it('') do
+  it('accepts a list of space-delimited strings and returns a list of true false results') do
     visit('/')
-    fill_in('', :with => '')
+    fill_in('source_w', :with => 'race')
+    fill_in('words_compare', :with => 'acre care rrrr')
     click_button('Send')
-    expect(page).to have_content('')
+    expect(page).to have_content('acre: yes; care: yes; rrrr: no')
   end
 end
