d8f908cb240a5347af8854397cb170c0000653b4
Michael & Drew <student@epicodus.com>
2015-04-23 12:11:06 -0700

    adds minimal code to pass equal length unit test

--- lib/anagramer.rb
+++ lib/anagramer.rb
@@ -1,12 +1,14 @@
 class String
   define_method(:anagramer) do |compare_to|
+    is_valid = false
     source_word = self
 
     if source_word.length == compare_to.length
 
     else
-
+      is_valid = false
     end
 
+    is_valid
   end
 end
