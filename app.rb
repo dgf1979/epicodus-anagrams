5d711c260d1b78aa625899a7da5ee6bf6dfb9dda
Michael & Drew <student@epicodus.com>
2015-04-23 14:38:52 -0700

    adds routes and views to implement method functionality in Sinatra

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ require('pry')
 get('/') do
   erb(:index)
 end
+
+get('/anagramer') do
+
+  @source_w = params.fetch('source_w')
+  @words_compare = params.fetch('words_compare')
+
+  @results = @source_w.anagramer(@words_compare)
+  erb(:results)
+end
